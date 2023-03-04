493c0db0346d488c67b7891ef6757b62cf6a6b95
1327016969 <77666948+1327016969@users.noreply.github.com>
2023-03-04 18:53:32 +0800

    Update test_case.py

--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from my_solution import solution
+
+from my_solution import get_word_vector
 
 
 # 测试用例
 def test_solution():
-    assert solution(8) == 9  # 判断输出结果，预期increment(8)应该为9
+    # 获取第二个token的输出词向量的维数
+    result = get_word_vector(1).shape
+    # 正确答案
+    correct_solution = torch.Size([768])
+    assert  result == correct_solution
